440d08553a1c83a210fa6547fc524af1e8881476
ravindra <ravindrachaubey410@gmail.com>
2024-08-28 22:00:32 +0530

    "Refactored HTML and CSS to use Tailwind CSS, removed Bootstrap, and simplified JavaScript code for weather API calls."

--- script.js
+++ script.js
@@ -1,134 +1,41 @@
 const options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': '2dfea15052msh5556fce1e61f501p17d012jsn47690de4e772',
-        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+        'x-rapidapi-key': '2dfea15052msh5556fce1e61f501p17d012jsn47690de4e772',
+        'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
     }
 };
+
 const getweather = (city) => {
-    cityName.innerHTML = city
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+    cityName.innerHTML = city;
+    fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=' + city, options)
         .then(response => response.json())
         .then(response => {
-            temp.innerHTML = response.temp
-            max_temp.innerHTML = response.max_temp
-            min_temp.innerHTML = response.min_temp
-            humidity.innerHTML = response.humidity
-            cloud_pct.innerHTML = response.cloud_pct
-            feels_like.innerHTML = response.feels_like
-            sunrise.innerHTML = response.sunrise
-            sunset.innerHTML = response.sunset
-            wind_speed.innerHTML = response.wind_speed
-            // wind_degrees.innerHTML = response.wind_degrees
-        }
-        )
+            const current = response.current;
+
+            // Populate data in respective cards
+            temp.innerHTML = current.temp_c;
+            min_temp.innerHTML = current.temp_c - 2; // Placeholder, adjust as needed
+            max_temp.innerHTML = current.temp_c + 2; // Placeholder, adjust as needed
+            pressure.innerHTML = current.pressure_mb;
+            visibility.innerHTML = current.vis_km * 1000; // Convert to meters
+            humidity.innerHTML = current.humidity;
+            cloud_pct.innerHTML = current.cloud;
+            feels_like.innerHTML = current.feelslike_c;
+            dew_point.innerHTML = current.dewpoint_c;
+            wind_speed.innerHTML = current.wind_kph;
+            wind_deg.innerHTML = current.wind_degree;
+
+            // Placeholder values for sunrise/sunset
+            sunrise.innerHTML = response.location.localtime.split(" ")[1];
+            sunset.innerHTML = response.location.localtime.split(" ")[1];
+        })
         .catch(err => console.error(err));
-}
+};
 
 submit.addEventListener("click", (e) => {
-    e.preventDefault()
-    getweather(city.value)
-})
-getweather("singrauli");
+    e.preventDefault();
+    getweather(city.value);
+});
 
-
-const kol = () => {
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=kolkata', options)
-        .then(response => response.json())
-        .then(response => {
-            temp_kol.innerHTML = response.temp
-            max_temp_kol.innerHTML = response.max_temp
-            min_temp_kol.innerHTML = response.min_temp
-            humidity_kol.innerHTML = response.humidity
-            cloud_pct_kol.innerHTML = response.cloud_pct
-            feels_like_kol.innerHTML = response.feels_like
-            sunrise_kol.innerHTML = response.sunrise
-            sunset_kol.innerHTML = response.sunset
-            wind_speed_kol.innerHTML = response.wind_speed
-
-        })
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-}
-kol("kolkata");
-
-
-const ben = () => {
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=bengaluru', options)
-        .then(response => response.json())
-        .then(response => {
-            temp_ben.innerHTML = response.temp
-            max_temp_ben.innerHTML = response.max_temp
-            min_temp_ben.innerHTML = response.min_temp
-            humidity_ben.innerHTML = response.humidity
-            cloud_pct_ben.innerHTML = response.cloud_pct
-            feels_like_ben.innerHTML = response.feels_like
-            sunrise_ben.innerHTML = response.sunrise
-            sunset_ben.innerHTML = response.sunset
-            wind_speed_ben.innerHTML = response.wind_speed
-
-        })
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-}
-ben("bengaluru");
-
-const del = () => {
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=delhi', options)
-        .then(response => response.json())
-        .then(response => {
-            temp_del.innerHTML = response.temp
-            max_temp_del.innerHTML = response.max_temp
-            min_temp_del.innerHTML = response.min_temp
-            humidity_del.innerHTML = response.humidity
-            cloud_pct_del.innerHTML = response.cloud_pct
-            feels_like_del.innerHTML = response.feels_like
-            sunrise_del.innerHTML = response.sunrise
-            sunset_del.innerHTML = response.sunset
-            wind_speed_del.innerHTML = response.wind_speed
-
-        })
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-}
-del("delhi");
-
-const luck = () => {
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=lucknow', options)
-        .then(response => response.json())
-        .then(response => {
-            temp_luck.innerHTML = response.temp
-            max_temp_luck.innerHTML = response.max_temp
-            min_temp_luck.innerHTML = response.min_temp
-            humidity_luck.innerHTML = response.humidity
-            cloud_pct_luck.innerHTML = response.cloud_pct
-            feels_like_luck.innerHTML = response.feels_like
-            sunrise_luck.innerHTML = response.sunrise
-            sunset_luck.innerHTML = response.sunset
-            wind_speed_luck.innerHTML = response.wind_speed
-
-        })
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-}
-luck("lucknow");
-
-const mum = () => {
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=mumbai', options)
-        .then(response => response.json())
-        .then(response => {
-            temp_mum.innerHTML = response.temp
-            max_temp_mum.innerHTML = response.max_temp
-            min_temp_mum.innerHTML = response.min_temp
-            humidity_mum.innerHTML = response.humidity
-            cloud_pct_mum.innerHTML = response.cloud_pct
-            feels_like_mum.innerHTML = response.feels_like
-            sunrise_mum.innerHTML = response.sunrise
-            sunset_mum.innerHTML = response.sunset
-            wind_speed_mum.innerHTML = response.wind_speed
-
-        })
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-}
-mum("bengaluru");
+getweather("Singrauli");
